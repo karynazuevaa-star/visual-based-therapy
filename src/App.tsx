5057576312d918bd2d17bfa8648787b09e3b0ff5
karynazuevaa-star <karynazuevaa@gmail.com>
2026-01-26 21:25:36 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import Home from "./Home";
-
 export default function App() {
   return (
-    <div style={{ padding: 24 }}>
-      <h1>App loaded</h1>
-      <Home />
+    <div
+      style={{
+        padding: 40,
+        fontSize: 24,
+        background: "#f5f5f5",
+        minHeight: "100vh",
+      }}
+    >
+      <h1>React работает ✅</h1>
+      <p>Если ты это видишь — всё ок.</p>
     </div>
   );
 }

715f25f4d504a636c04ecba55f1d00b0486d03df
karynazuevaa-star <karynazuevaa@gmail.com>
2026-02-07 12:21:28 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -60,3 +60,16 @@ try {
 } catch (err) {
   showFatal(err);
 }
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
